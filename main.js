9901a9c87c375562122f08244c8bd9b5f90e283f
Emily DeWitt <emilykdewitt@gmail.com>
2019-03-02 11:09:11 -0600

    Added name function to .js file

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello!')
+//George Smith
+//Sam Smith
+//John Doe
+//Tori Amos
+//Britney Spears
+
+
+// const firstName = 'Emily';
+// const lastName = 'DeWitt';
+// console.log(firstName + ' ' + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter('Emily', 'DeWitt');
+namePrinter('John', 'Doe');
+namePrinter('Sally', 'Smith');
